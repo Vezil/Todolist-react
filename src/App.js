7cf253089fa7913cdd1b8ff86fca6edcad215f2e
Vezil <SzymonWojaczek@wp.pl>
2019-12-12 17:50:51 +0100

    starting template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="Todo-container">
+        <input type="text" className="todo-input" placeholder="What do you have to do ?"/>
+          <div className="todo-item">
+            <input type="checkbox"/>
+            <div className="todo-item-label">Todo Item1</div>
+            <div className="remove-item">&times;</div>
+          </div>
+
+          <div className="todo-item">
+            <input type="checkbox"/>
+            <div className="todo-item-label">Todo Item1</div>
+            <div className="remove-item">&times;</div>
+          </div>
+
+      </div>
+
+
+
     </div>
   );
+  
+  }
+
+  state = {
+    todos:[
+      {
+        'id':1,
+        'title':'Matero',
+        'done': false,
+      },
+      {
+        'id':2,
+        'title':'Bombila',
+        'done': false,
+      },
+    ]
+  }
 }
 
 export default App;
